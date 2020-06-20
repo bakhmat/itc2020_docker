f6cda018ef04115375f0fcf6451b642114fc75e2
Andrey Bakhmat <a.bakhmat@gmail.com>
2020-06-20 14:58:27 +0300

    now main.py prints html source of urls passed as list of arguments

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import sys
-from time import sleep
+import urllib.request
+# from time import sleep
 
 if __name__ == "__main__":
-    num = int(sys.argv[1])
-    for i in range(num):
-        # print (str(i)+" "+str(i*i*i))
-        print(i)
-        sleep(1)
+    url_list = sys.argv
+    for url in url_list:
+        try:
+            fp = urllib.request.urlopen(url)
+            mybytes = fp.read()
+            html_output = mybytes.decode("utf8")
+            fp.close()
+        except:
+            html_output = ""
+
+        print("### URL source ######################################################### \n\n")
+        print(html_output)
+
+# if __name__ == "__main__":
+#     num = int(sys.argv[1])
+#     for i in range(num):
+#         print (str(i)+" "+str(i*i*i))
+#         sleep(1)
